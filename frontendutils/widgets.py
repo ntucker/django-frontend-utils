547fc5bd76b450bd8ae8897127c8c16c4026ddda
Nathaniel Tucker <me@ntucker.me>
2014-01-04 21:53:02 +0000

    Use force_text for py3 compat

--- frontendutils/widgets.py
+++ frontendutils/widgets.py
@@ -1,8 +1,8 @@
 from django import forms
 from django.utils.safestring import mark_safe
+from django.utils.encoding import force_text
 from django.forms.util import flatatt
 from django.utils.html import escape, conditional_escape
-from django.utils.encoding import force_unicode
 
 
 class DateTimePicker(forms.DateTimeInput):
@@ -27,7 +27,7 @@ class TagInput(forms.TextInput):
         if value is None: value = ''
         final_attrs = self.build_attrs(attrs, name=name)
         tag_class = u" ".join((final_attrs.get('class', ""), 'tag-list'))
-        ini_element = '<div class="tag-data">%s</div>' % force_unicode(value) if value else ""
+        ini_element = '<div class="tag-data">%s</div>' % force_text(value) if value else ""
         return mark_safe(u"""<i class="icon-tags pull-left" style="position:relative;bottom:-5px;"></i><div data-id="%s" class="%s" data-suggestions="%s" >%s<div class="tags"></div></div>
         <input type='hidden'%s />""" % (final_attrs['id'], tag_class, u",".join(self.suggestions), ini_element, flatatt(final_attrs)))
     class Media:
