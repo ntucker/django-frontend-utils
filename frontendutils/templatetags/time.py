fd6a2813da9d19aebaedf75b7da37712a521c0ef
Nathaniel Tucker <me@ntucker.me>
2014-11-16 00:52:13 +0000

    Add microdata inclusion to time tag

--- frontendutils/templatetags/time.py
+++ frontendutils/templatetags/time.py
@@ -6,10 +6,11 @@ from django.utils.timezone import localtime
 register = template.Library()
 
 @register.inclusion_tag('utils/tags/show_time.html')
-def show_time(time, time_display=None, pubdate=False):
+def show_time(time, time_display=None, pubdate=False, itemprop=None):
     context = {'time': time, 'pubdate':pubdate}
     if time_display:
         context['time_display'] = time_display
     else:
         context['time_display'] = date(localtime(time), settings.DATETIME_FORMAT) if time else ""
+    context['itemprop'] = itemprop
     return context
